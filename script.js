42507cbb40249e2942e5683147b23d7353b1592f
Adia Wahyu Dwi Prasetya <54004272+adiakdcode@users.noreply.github.com>
2020-05-24 06:56:45 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-new Typed('#typed',{
-        strings : ['Selamat Hari Raya Idul Fitri 1 Syawal 1441 H. Mohon maaf lahir dan batin, Semoga Amal ibadah kita diterima oleh Allah SWT...'],
-        typeSpeed : 70,
-        delaySpeed : 70,
-        loop : true
-});
-
